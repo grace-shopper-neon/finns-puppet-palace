dd1391e96be1206c108016f34f804bfcf6c6be71
Jack-Thornhill <phisherman10@hotmail.com>
2020-04-22 15:56:35 -0500

    render

--- client/components/SingleUser.js
+++ client/components/SingleUser.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {fetchSingleUser} from '../store/singleUser'
 
 class SingleUser extends React.Component {
   componentDidMount() {
@@ -7,16 +8,32 @@ class SingleUser extends React.Component {
   }
 
   render() {
-    return <div />
+    const fullName = this.props.fullName
+    const email = this.props.email
+    const isAdmin = this.props.isAdmin
+    const googleId = this.props.googleId
+
+    return (
+      <div className="singleUser">
+        <h1 className="fullName">{fullName}</h1>
+        <h1 className="email">{email}</h1>
+        <div className="adminCheck">{isAdmin ? 'Admin' : ''}</div>
+        <div className="googleId" />
+      </div>
+    )
   }
 }
 
 const mapState = state => {
-  return {}
+  return {
+    singleUser: state.singleUser
+  }
 }
 
 const mapDispatch = dispatch => {
-  return {}
+  return {
+    getSingleUser: id => dispatch(fetchSingleUser(id))
+  }
 }
 
 export default connect(mapState, mapDispatch)(SingleUser)
