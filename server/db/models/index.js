2d4dc96e6df0e7bba8e265ab0c17efcee7557d8c
Aidan Garton <amgb2018@mymail.pomona.edu>
2020-04-22 10:23:02 -0500

    Done with models part 1/2
    c
    s

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,46 @@
+const db = require('./database')
+
+const Order = require('./order')
+const OrderList = require('./orderList')
+const Cart = require('./cart')
+
+const Session = require('./session')
 const User = require('./user')
+const Review = require('./review')
+const Product = require('./product')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+// Model Associations
+Cart.hasMany(OrderList)
+OrderList.belongsTo(Cart)
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+Cart.hasOne(Session)
+Session.belongsTo(Cart)
+
+User.hasMany(Review)
+Review.belongsTo(User)
+
+User.hasOne(Session)
+Session.belongsTo(User)
+
+User.hasMany(Order)
+Order.belongsTo(User)
+
+Product.hasMany(Review)
+Review.belongsTo(Product)
+
+OrderList.hasMany(Product)
+Product.belongsTo(OrderList)
+
+Order.hasMany(OrderList)
+OrderList.belongsTo(Order)
+
 module.exports = {
-  User
+  db,
+  Order,
+  OrderList,
+  Cart,
+  Session,
+  User,
+  Review,
+  Product
 }
