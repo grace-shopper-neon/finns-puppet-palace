752f42478bf0a67e120cbd7069b772b7e75b6059
Aidan Garton <amgb2018@mymail.pomona.edu>
2020-04-22 11:49:53 -0500

    created singleProduct Component file

--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,7 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
+import SingleProduct from './components/SingleProduct'
 
 /**
  * COMPONENT
@@ -19,11 +20,12 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route path="/sp" component={SingleProduct} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/sp" component={SingleProduct} />
         {isLoggedIn && (
           <Switch>
-            {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
           </Switch>
         )}
--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -1,4 +1,4 @@
-const db = require('./database')
+const db = require('../db')
 const Sequelize = require('sequelize')
 
 const Cart = db.define('cart', {
