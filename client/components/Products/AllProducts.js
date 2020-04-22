1a287e98e6600c192d11fee2b6dbadef2598fa29
Isra Khan <israkhan2@gmail.com>
2020-04-22 11:39:19 -0500

    setup barebones render -- running into issues with dispatch

--- client/components/Products/AllProducts.js
+++ client/components/Products/AllProducts.js
@@ -3,10 +3,33 @@ import {fetchProducts} from '../../store/productsReducer'
 import {connect} from 'react-redux'
 
 export class AllProducts extends React.Component {
+  componentDidMount() {
+    console.log(this.props)
+    this.props.getProducts()
+  }
+
   render() {
+    if (!this.props.products) {
+      return (
+        <div>
+          <h1>Products</h1>
+        </div>
+      )
+    }
     return (
       <div>
         <h1>Products</h1>
+        <div>
+          {this.props.products.map(product => {
+            return (
+              <div key={product.id}>
+                <h2>{product.name}</h2>
+                <img src={product.imageUrl} />
+                <h4>{product.price}</h4>
+              </div>
+            )
+          })}
+        </div>
       </div>
     )
   }
@@ -16,8 +39,10 @@ const mapState = state => ({
   products: state.products
 })
 
-const mapDispatch = dispatch => ({
-  getProducts: () => dispatch(fetchProducts())
-})
+const mapDispatch = dispatch => {
+  return {
+    getProducts: () => dispatch(fetchProducts())
+  }
+}
 
 export default connect(mapState, mapDispatch)(AllProducts)
