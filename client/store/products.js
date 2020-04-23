7c430f60dd729dcad522f75099deb66435084459
Isra Khan <israkhan2@gmail.com>
2020-04-23 10:25:58 -0500

    removing stub data from products.js

--- client/store/products.js
+++ client/store/products.js
@@ -1,3 +1,4 @@
+import Axios from 'axios'
 const SET_PRODUCTS = 'SET_PRODUCTS'
 
 export const setProducts = products => ({
@@ -5,36 +6,36 @@ export const setProducts = products => ({
   products
 })
 
-const imageUrl =
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Creative-Tail-Animal-dog.svg/256px-Creative-Tail-Animal-dog.svg.png'
+// const imageUrl =
+//   'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Creative-Tail-Animal-dog.svg/256px-Creative-Tail-Animal-dog.svg.png'
 
 export const fetchProducts = () => {
-  return dispatch => {
+  return async dispatch => {
     try {
-      // const { data } = await Axios.get("/api/products");
-      const data = [
-        {
-          id: 1,
-          name: 'Dragon',
-          description: 'get three dragons for the price of one!',
-          imageUrl: imageUrl,
-          price: 1000
-        },
-        {
-          id: 1,
-          name: 'Cat',
-          description: 'get three cats for the price of one!',
-          imageUrl: imageUrl,
-          price: 10000
-        },
-        {
-          id: 1,
-          name: 'Dog',
-          description: 'get three dogs for the price of one!',
-          imageUrl: imageUrl,
-          price: 40000
-        }
-      ]
+      const {data} = await Axios.get('/api/products')
+      // const data = [
+      //   {
+      //     id: 1,
+      //     name: 'Dragon',
+      //     description: 'get three dragons for the price of one!',
+      //     imageUrl: imageUrl,
+      //     price: 1000
+      //   },
+      //   {
+      //     id: 1,
+      //     name: 'Cat',
+      //     description: 'get three cats for the price of one!',
+      //     imageUrl: imageUrl,
+      //     price: 10000
+      //   },
+      //   {
+      //     id: 1,
+      //     name: 'Dog',
+      //     description: 'get three dogs for the price of one!',
+      //     imageUrl: imageUrl,
+      //     price: 40000
+      //   }
+      // ]
 
       dispatch(setProducts(data))
     } catch (err) {
