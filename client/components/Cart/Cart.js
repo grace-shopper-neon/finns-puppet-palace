c0ea221d05c79f9ae1b80fdededbdf8371ee00a8
Isra Khan <israkhan2@gmail.com>
2020-04-23 16:24:04 -0500

    set up fake data to  validate store is working

--- client/components/Cart/Cart.js
+++ client/components/Cart/Cart.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {fetchCartOrders} from '../../store/cart'
 
-export default class Cart extends React.Component {
-  componentDidMount() {}
+export class Cart extends React.Component {
+  componentDidMount() {
+    //get OrderLists with cartID with thunk
+    const cartId = 1
+    this.props.getOrders(cartId)
+  }
 
   render() {
-    const cartId = 1
+    console.log(this.props.cartOrderLists)
     return (
       <div>
         <h1>Your Cart</h1>
-        <div id="orderLists">{/* <OrderList /> */}</div>
+        <div id="orderLists">
+          {this.props.cartOrderLists.map(order => {
+            return (
+              <div key={order.id}>
+                <div>{order.quantity}</div>
+              </div>
+            )
+          })}
+        </div>
         <div>Subtotal:</div>
         <div>Checkout</div>
         <div>Continue Shopping</div>
@@ -17,3 +30,13 @@ export default class Cart extends React.Component {
     )
   }
 }
+
+const mapState = state => ({
+  cartOrderLists: state.cart
+})
+
+const mapDispatch = dispatch => ({
+  getOrders: id => dispatch(fetchCartOrders(id))
+})
+
+export default connect(mapState, mapDispatch)(Cart)
