65389d503f6ef4cacbf7c956f748b26635c40568
Aidan Garton <amgb2018@mymail.pomona.edu>
2020-04-24 14:48:36 -0500

    commit

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -61,36 +61,6 @@ const Navbar = ({handleClick, isLoggedIn}) => {
                   </a>
                 </Link>
               </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                  <div className="dropdown-divider"></div>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </div>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#">
-                  Disabled
-                </a>
-              </li>
             </ul>
           </div>
         )}
--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router()
 router.use('/users', require('./users'))
 router.use('/products', require('./products'))
 router.use('/reviews', require('./reviews'))
+router.use('/orderLists', require('./orderLists'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
