f9480655b2f8a76b189c52321f6c67b28fc253e2
Finn <finnterdal@gmail.com>
2020-04-27 16:00:39 -0500

    sessions sessions sessions!

--- server/api/orderLists.js
+++ server/api/orderLists.js
@@ -4,6 +4,28 @@ const Product = require('../db/models/product')
 
 router.get('/:cartId', async (req, res, next) => {
   try {
+    console.log('req.user', req.user)
+    console.log('req.session', req.session)
+
+    if (!req.user) {
+      console.log('user is not logged in')
+
+      // Strategy One: Add a cart id to the session. Edit the
+      // cart table when they add to or remove from the cart
+      // if (!req.session.cartId) {
+      //   const cart = await Cart.create();
+      //   req.session.cartId = cart.id;
+      // }
+
+      // Strategy Two: For guests, store the entire cart on the session.
+      // (It would be an array or something like that instead of a number.)
+      if (!req.session.cart) {
+        req.session.cart = 1
+      } else {
+        req.session.cart++
+      }
+    }
+
     const orderLists = await OrderList.findAll({
       where: {cartId: req.params.cartId},
       include: Product
--- server/api/users.js
+++ server/api/users.js
@@ -1,33 +1,36 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
 
-router.get('/', async (req, res, next) => {
-  try {
-    const currUser = req.user // this is the currently authenticated user
-    if (currUser) {
-      // if the user is an admin, they should receive all user info
-      if (currUser.isAdmin) {
-        const users = await User.findAll()
-        return res.json(users)
-      }
+// isAdmin gatekeeper
+const isAdmin = (req, res, next) => {
+  const currUser = req.user // this is the currently authenticated user
+  if (currUser) {
+    // if the user is an admin, they should receive all user info
+    if (currUser.isAdmin) {
+      next()
+    } else {
+      next(new Error('Not Authorized'))
     }
+  } else {
     // otherwise only send the id and email info
-    res.status(403).send()
+    res.status(403)
+    next(new Error('Not Authenticated'))
+  }
+}
+
+router.get('/', isAdmin, async (req, res, next) => {
+  try {
+    const users = await User.findAll()
+    return res.json(users)
   } catch (err) {
     next(err)
   }
 })
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', isAdmin, async (req, res, next) => {
   try {
-    const currUser = req.user
-    if (currUser) {
-      if (currUser.isAdmin) {
-        const user = await User.findByPk(req.params.id)
-        return res.json(user)
-      }
-    }
-    res.status(403).send()
+    const user = await User.findByPk(req.params.id)
+    return res.json(user)
   } catch (err) {
     next(err)
   }
