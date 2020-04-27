1deb8c43e827eb2a079b36fe51654f21686231de
Isra Khan <israkhan2@gmail.com>
2020-04-27 12:07:38 -0500

    making container fluid and first attempt card organization with rows/col

--- client/components/Products/AllProducts.js
+++ client/components/Products/AllProducts.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {fetchProducts} from '../../store/products'
 import {connect} from 'react-redux'
 import ProductCard from './ProductCard'
+import {Row, Col, Grid} from 'react-bootstrap'
 
 export class AllProducts extends React.Component {
   componentDidMount() {
@@ -10,15 +11,23 @@ export class AllProducts extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Products</h1>
-        <div className="container mt-5">
-          <div className="card-deck">
-            {this.props.products.map(product => (
+      <div className="container-fluid mt-5">
+        {/* <div className="card-deck"> */}
+        {this.props.products.map((product, index) =>
+          !((index + 1) % 4) || index === 0 ? (
+            <div className="row">
+              {' '}
+              <div className="col-md-4 cols-sm-6 col-xs-12">
+                <ProductCard key={product.id} product={product} />
+              </div>
+            </div>
+          ) : (
+            <div className="col-md-4 cols-sm-6 col-xs-12">
               <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
+            </div>
+          )
+        )}
+        {/* </div> */}
       </div>
     )
   }
--- client/components/Products/ProductCard.js
+++ client/components/Products/ProductCard.js
@@ -4,17 +4,19 @@ import priceConv from '../../utility/priceConversion'
 
 export default function ProductCard(props) {
   const product = props.product
+  const imageUrl =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Creative-Tail-Animal-dog.svg/256px-Creative-Tail-Animal-dog.svg.png'
   return (
     //added classNames that are used in Bootstrap for a demo .. do not remove
     <div className="card-body" key={product.id}>
       <div className="card">
-        <img className="img-fluid" src={product.imageUrl} />
-        <h3 className="card-title">{product.name}</h3>
-        <h5 className="card-subtitle">{`${priceConv(product.price)}`}</h5>
-        <p>{product.description}</p>
-        <Link to={`/products/${product.id}`} className="card-link">
-          More Details
-        </Link>
+        <div>
+          <Link to={`/products/${product.id}`} className="card-link">
+            <img className="card-img-top" src={product.imageUrl} />
+          </Link>
+        </div>
+        <h4 className="card-title">{product.name}</h4>
+        <h6 className="card-subtitle mt-1">{`${priceConv(product.price)}`}</h6>
       </div>
     </div>
   )
