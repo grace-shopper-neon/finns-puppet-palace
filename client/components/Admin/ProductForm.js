fa2ed26f66fe5790ce716d8fc09165f5690ded2a
Ian Logan <itlogan9@gmail.com>
2020-04-28 16:00:25 -0500

    Polish add product form

--- client/components/Admin/ProductForm.js
+++ client/components/Admin/ProductForm.js
@@ -5,12 +5,13 @@ class ProductForm extends React.Component {
     super()
     this.state = {
       name: '',
-      color: '',
-      animal: '',
+      color: 'red',
+      animal: 'pig',
       price: '',
       description: ''
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange(e) {
@@ -19,69 +20,85 @@ class ProductForm extends React.Component {
     })
   }
 
+  handleSubmit(e) {
+    e.preventDefault()
+  }
+
   render() {
     const {name, color, animal, price, description} = this.state
     return (
-      <div className="container mt-5">
+      <div className="w-50 mt-5 mx-auto">
         <h3>Add Product</h3>
-        <form>
-          <div className="form-group">
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group d-block">
             <label htmlFor="name">Name: </label>
             <input
+              id="name"
               className="form-control"
               type="text"
               value={name}
               onChange={this.handleChange}
               name="name"
-              placeholder="Enter name"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="animal">Animal: </label>
-            <input
-              className="form-control"
-              type="text"
-              value={animal}
-              onChange={this.handleChange}
-              name="animal"
-              placeholder="Enter a type of animal"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="color">Color: </label>
-            <input
-              className="form-control"
-              type="text"
-              value={color}
-              onChange={this.handleChange}
-              name="color"
-              placeholder="Enter a color"
+              placeholder="Mr. Bojangles"
             />
           </div>
           <div className="form-group">
             <label htmlFor="price">Price: </label>
             <input
+              id="price"
               className="form-control"
-              type="text"
+              type="number"
               value={price}
               onChange={this.handleChange}
               name="price"
-              placeholder="Enter a price"
+              placeholder="Enter a price in cents"
             />
+            <small>I'm sorry</small>
           </div>
           <div className="form-group">
+            <label htmlFor="animal">Animal: </label>
+            <select
+              id="animal"
+              value={animal}
+              onChange={this.handleChange}
+              name="animal"
+            >
+              <option value="pig">pig</option>
+              <option value="dog">dog</option>
+              <option value="cat">cat</option>
+              <option value="penguin">penguin</option>
+              <option value="alligator">alligator</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="color">Color: </label>
+            <select
+              id="color"
+              value={color}
+              onChange={this.handleChange}
+              name="color"
+            >
+              <option value="red">red</option>
+              <option value="blue">blue</option>
+              <option value="green">green</option>
+              <option value="orange">orange</option>
+              <option value="yellow">yellow</option>
+            </select>
+          </div>
+
+          <div className="form-group d-block">
             <label htmlFor="description">Description: </label>
             <textarea
+              id="description"
               className="form-control"
               type="textarea"
               value={description}
               onChange={this.handleChange}
               name="description"
-              placeholder="Enter a description"
+              placeholder="What's this puppet's story?"
               row="3"
             ></textarea>
           </div>
-          <input type="file" className="form-control-file"></input>
           <button className="btn btn-primary" type="submit">
             Submit
           </button>
