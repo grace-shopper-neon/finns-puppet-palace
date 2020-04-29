45eaffb59cd066a95c016cce51b3a75f8581efde
Isra Khan <israkhan2@gmail.com>
2020-04-29 08:26:28 -0500

    Renaming react component

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import SingleUser from './SingleUser'
-import OrderHistory from './OrderHistory'
+import OrderHistory from './SingleUserOrders'
 
 /**
  * COMPONENT
