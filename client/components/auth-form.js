df5a460091ca005b9246230ad0856d6f4fb31680
Ian Logan <itlogan9@gmail.com>
2020-04-28 20:03:38 -0500

    Add ids to forms so labels work

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -17,17 +17,17 @@ const AuthForm = props => {
             <label htmlFor="firstName">
               <small>First Name</small>
             </label>
-            <input name="firstName" type="text" />
+            <input id="firstName" name="firstName" type="text" />
           </div>
         ) : (
           ''
         )}
         {displayName === 'Sign Up' ? (
           <div>
-            <label htmlFor="firstName">
+            <label htmlFor="lastName">
               <small>Last Name</small>
             </label>
-            <input name="lastName" type="text" />
+            <input id="lastName" name="lastName" type="text" />
           </div>
         ) : (
           ''
@@ -36,13 +36,13 @@ const AuthForm = props => {
           <label htmlFor="email">
             <small>Email</small>
           </label>
-          <input name="email" type="text" />
+          <input id="email" name="email" type="text" />
         </div>
         <div>
           <label htmlFor="password">
             <small>Password</small>
           </label>
-          <input name="password" type="password" />
+          <input id="password" name="password" type="password" />
         </div>
         <div>
           <button type="submit">{displayName}</button>
